0fb3674972dae7c6e17964b3fff75c9c17b362d4
Griffin Scott <griffinoscott@gmail.com>
2023-08-31 21:49:59 -0700

    moved css files to public folder

--- routes/route.js
+++ routes/route.js
@@ -4,40 +4,33 @@ const path = require('path');
 
 
 router.get('/', function(req, res) {
-
     let filePath = path.join(__dirname, '/../views/HomePage/home.html')
     res.sendFile(filePath);
-
-});
-
-router.get('/unidades', function(req, res) {
-
-    let filePath = path.join(__dirname, '/../views/ProductPage/Unid/unidades.html')
-    res.sendFile(filePath);
-
 });
 
 router.get('/nosotros', function(req, res) {
-    res.send('nosotros page');
+    let filePath = path.join(__dirname, '/../views/Nosotros/nosotros.html')
+    res.sendFile(filePath);
 });
 
 router.get('/dealer', function(req, res) {
-    res.send('dealer page');
+    let filePath = path.join(__dirname, '/../views/DealerPage/dealer.html')
+    res.sendFile(filePath);
 });
 
 router.get('/dealer/locator', function(req, res) {
-    res.send('dealer locator page');
+    let filePath = path.join(__dirname, '/../views/DealerLocator/dealerlocator.html')
+    res.sendFile(filePath);
 });
 
-router.get('/products/', function(req, res) {
-    res.send('products list page');
+router.get('/unidades', function(req, res) {
+    let filePath = path.join(__dirname, '/../views/ProductPage/Unid/unidades.html')
+    res.sendFile(filePath);
 });
 
-router.get('/products/:name', function(req, res) {
-    res.send(`product ${req.params.name}  page`);
+router.get('/unidades/:name', function(req, res) {
+    res.send(`unidades ${req.params.name}  page`);
 });
-
-
 
 
 module.exports = router
